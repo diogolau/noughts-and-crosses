ad501acdbbfb93b79d81455d30930ea1f6c0a5e8
Alexandre Mac <bravomac@MacBook-Air-de-Alexandre-2.local>
2023-10-27 17:29:42 -0300

    better layout

--- server_config.py
+++ server_config.py
@@ -1,57 +1,47 @@
 import tkinter as tk
-from server import initiate_server
 
-class ServerConfigScreen:
+class ServerConfigurationScreen:
     def __init__(self, master):
         self.master = master
         master.title("Server Configuration")
 
-        self.label_host = tk.Label(master, text="Host:")
-        self.label_host.grid(row=0, column=0)
+        self.host_label = tk.Label(master, text="Host Port:")
+        self.host_label.pack()
 
-        self.entry_host = tk.Entry(master)
-        self.entry_host.grid(row=0, column=1)
+        self.host_entry = tk.Entry(master, highlightbackground="white")
+        self.host_entry.pack()
 
-        self.label_port = tk.Label(master, text="Port:")
-        self.label_port.grid(row=1, column=0)
+        self.send_button = tk.Button(master, text="Send", command=self.send_values)
+        self.send_button.pack()
 
-        self.entry_port = tk.Entry(master)
-        self.entry_port.grid(row=1, column=1)
+        self.init_button = tk.Button(master, text="Initialize Server", command=self.initialize_server)
+        self.init_button.pack()
 
-        self.button_initialize = tk.Button(master, text="Initialize Server", command=self.initialize_server)
-        self.button_initialize.grid(row=2, column=0)
+        self.kill_button = tk.Button(master, text="Kill Server", command=self.kill_server)
+        self.kill_button.pack()
 
-        self.button_kill = tk.Button(master, text="Kill Server", command=self.kill_server)
-        self.button_kill.grid(row=2, column=1)
+        self.logs_textarea = tk.Text(master, highlightbackground="white")
+        self.logs_textarea.pack()
 
-        self.text_logs = tk.Text(master, height=10, width=40)
-        self.text_logs.grid(row=3, columnspan=2)
+        self.summary_textarea = tk.Text(master, highlightbackground="white")
+        self.summary_textarea.pack()
 
-        self.text_requests_responses = tk.Text(master, height=10, width=40)
-        self.text_requests_responses.grid(row=4, columnspan=2)
+        self.server_state_label = tk.Label(master, text="Server State: ")
+        self.server_state_label.pack()
 
-        self.label_server_state = tk.Label(master, text="Server State: ")
-        self.label_server_state.grid(row=5, column=0)
-
-        self.label_server_state_value = tk.Label(master, text="Not Running")
-        self.label_server_state_value.grid(row=5, column=1)
+    def send_values(self):
+        host_port = self.host_entry.get()
+        # Logic to handle host port
 
     def initialize_server(self):
-        host = self.entry_host.get()
-        port = self.entry_port.get()
-        if host and port:
-            initiate_server(host, port)
-            self.label_server_state_value.config(text="Running")
-            self.text_logs.insert(tk.END, "Server initialized\n")
-        else:
-            self.text_logs.insert(tk.END, "Please provide host and port\n")
+        # Logic to initialize the server here
+        pass
 
     def kill_server(self):
-        self.label_server_state_value.config(text="Not Running")
-        self.text_logs.insert(tk.END, "Server killed\n")
-
+        # Logic to kill the server here
+        pass
 
 if __name__ == '__main__':
     root = tk.Tk()
-    my_gui = ServerConfigScreen(root)
+    my_gui = ServerConfigurationScreen(root)
     root.mainloop()
